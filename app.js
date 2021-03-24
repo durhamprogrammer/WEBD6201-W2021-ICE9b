22a68f7928bdc0160f5e7d601138974c327a3e37
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-03-24 10:15:17 -0400

    updated app.ts to correct heroku bug

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'Public')));
+app.use(express.static(path.join(__dirname, 'Client')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use('/', indexRouter);
 app.use(function (req, res, next) {
