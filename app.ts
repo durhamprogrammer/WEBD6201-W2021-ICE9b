fefbdcdcd49c8445289570825dd531a99b928555
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-03-24 09:31:37 -0400

    added previous project files

--- app.ts
+++ app.ts
@@ -6,20 +6,21 @@ import cookieParser = require('cookie-parser');
 import logger = require('morgan');
 
 // routing
-let indexRouter = require('./routes/index');
+let indexRouter = require('./Routes/index');
 
 // web app instance
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'Views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Client')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', indexRouter);
 
