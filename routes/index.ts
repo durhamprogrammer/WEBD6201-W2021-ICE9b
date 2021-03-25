73bdcef17430a4fd27926de1b7dc622a78bce3e3
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-03-25 08:47:39 -0400

    added content injection and link highlighting

--- routes/index.ts
+++ routes/index.ts
@@ -4,14 +4,51 @@ let router = express.Router();
 /* GET home page - default route. */
 router.get('/', function(req, res, next) 
 {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home', page: 'home' });
 });
 
 /* GET home page - home route. */
 router.get('/home', function(req, res, next) 
 {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home', page: 'home'  });
 });
+
+/* GET about page - about route. */
+router.get('/about', function(req, res, next) 
+{
+  res.render('index', { title: 'About Us', page: 'about'  });
+});
+
+/* GET projects page - projects route. */
+router.get('/projects', function(req, res, next) 
+{
+  res.render('index', { title: 'Our Projects', page: 'projects'  });
+});
+
+/* GET services page - services route. */
+router.get('/services', function(req, res, next) 
+{
+  res.render('index', { title: 'Our Services', page: 'services'  });
+});
+
+/* GET contact page - contact route. */
+router.get('/contact', function(req, res, next) 
+{
+  res.render('index', { title: 'Contact Us', page: 'contact'  });
+});
+
+/* GET login page - login route. */
+router.get('/login', function(req, res, next) 
+{
+  res.render('index', { title: 'Login', page: 'login'  });
+});
+
+/* GET register page - register route. */
+router.get('/register', function(req, res, next) 
+{
+  res.render('index', { title: 'Register', page: 'register'  });
+});
+
 
 
 module.exports = router;
